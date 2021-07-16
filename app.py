d9927245675db6dd46608ae29b00845603ee6c4a
Roman Oechslin <romanoech@hotmail.com>
2021-07-16 18:52:54 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
-from flask import Flask,render_template,flash, redirect,url_for,session,logging,request
+import os
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+import hashlib
 
+from dbconnect import connection
+
+
+file_path = os.path.abspath(os.getcwd())+"\database.db"
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/bozkurt/Desktop/login-register-form/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 db = SQLAlchemy(app)
 
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    email = db.Column(db.String(120))
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(50))
+    email = db.Column(db.String(150))
+    password = db.Column(db.String(32))
+    password_clear = db.Column(db.String(32))
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-
-@app.route("/login",methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         uname = request.form["uname"]
         passw = request.form["passw"]
-        
-        login = user.query.filter_by(username=uname, password=passw).first()
+
+        login = User.query.filter_by(username=uname, password=passw).first()
+
         if login is not None:
             return redirect(url_for("index"))
     return render_template("login.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -35,13 +59,16 @@ def register():
         uname = request.form['uname']
         mail = request.form['mail']
         passw = request.form['passw']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
 
-        register = user(username = uname, email = mail, password = passw)
+        register = User(username=uname, email=mail, password=hashlib.md5(passw.encode('utf-8')).hexdigest(), password_clear='', firstname=firstname, lastname=lastname)
         db.session.add(register)
         db.session.commit()
 
         return redirect(url_for("login"))
     return render_template("register.html")
+
 
 if __name__ == "__main__":
     db.create_all()
